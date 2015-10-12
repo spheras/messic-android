354ca9061d93336924685509263fa63d86fff52d
Fco Javier Coira Pérez <fjcoira@gmail.com>
2015-10-12 21:18:23 +0200

    Play/pause button

--- messic-smarttv/src/main/java/org/messic/android/messic_tv/activities/MainActivity.java
+++ messic-smarttv/src/main/java/org/messic/android/messic_tv/activities/MainActivity.java
@@ -15,9 +15,13 @@
 package org.messic.android.messic_tv.activities;
 
 import android.app.Activity;
+import android.content.ComponentName;
+import android.content.Context;
+import android.media.AudioManager;
 import android.os.Bundle;
 
 import org.messic.android.messic_tv.R;
+import org.messic.android.messic_tv.receivers.RemoteControlReceiver;
 
 /*
  * MainActivity class that loads MainFragment
@@ -34,6 +38,8 @@ public class MainActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        AudioManager manager = (AudioManager)getApplicationContext().getSystemService(Context.AUDIO_SERVICE);
+        manager.registerMediaButtonEventReceiver(new ComponentName(getPackageName(), RemoteControlReceiver.class.getName()));
 
     }
 
